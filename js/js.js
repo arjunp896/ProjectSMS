bd59e7b9c0af28572fa6c68f505deca5560b9624
Arjun Patel <arjunp896@gmail.com>
2021-11-17 16:42:45 -0500

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,53 @@
+var slideIndex = [1,1];
+var slideId = ["owl"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+	/*let product = document.getElementsByClassName('product')
+	let owl = Math.ceil(product.length/4);
+	let l = 0;
+	let movePer = 25.34;
+	let maxMove = 203;
+	// mobile_view	
+	let mob_view = window.matchMedia("(max-width: 768px)");
+	if (mob_view.matches)
+	 {
+	 	movePer = 50.36;
+	 	maxMove = 504;
+	 }
+
+	let right_mover = ()=>{
+		l = l + movePer;
+		if (product == 1){l = 0; }
+		for(const i of product)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.left = '-' + l + '%';
+		}			
+	}
+	let left_mover = ()=>{
+		l = l - movePer;
+		if (l<=0){l = 0;}
+		for(const i of product){
+			if (owl>1){
+				i.style.left = '-' + l + '%';
+			}
+		}
+	}
+	move[1].onclick = ()=>{right_mover();}
+	move[0].onclick = ()=>{left_mover();}*/
